4430f9b1b0ca3375a323ce8617c9e008236e946e
betina17 <rsustrean@gmail.com>
2024-06-06 00:37:08 +0300

    second commit-added Hello Controller for Render visualization

--- src/main/java/com/example/experiment/ExperimentApplication.java
+++ src/main/java/com/example/experiment/ExperimentApplication.java
@@ -2,6 +2,8 @@ package com.example.experiment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class ExperimentApplication {
@@ -10,4 +12,12 @@ public class ExperimentApplication {
 		SpringApplication.run(ExperimentApplication.class, args);
 	}
 
+}
+
+@RestController
+class HelloController {
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello, World!";
+	}
 }
